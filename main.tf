73ac05de257eb0ec1ecb8251dcbe9a370dfa4761
Jaime <jsalcedo87@gmail.com>
2022-12-01 12:25:43 +0000

    fix: adding partition input parameter

--- main.tf
+++ main.tf
@@ -167,6 +167,7 @@ module "webhook" {
     
   lambda_subnet_ids         = var.lambda_subnet_ids
   lambda_security_group_ids = var.lambda_security_group_ids
+  aws_partition             = var.aws_partition
 
   log_type  = var.log_type
   log_level = var.log_level
