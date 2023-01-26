c316445df8441ac5eecff158df8383bef4653e40
jsalcedo1987 <jsalcedo87@gmail.com>
2023-01-26 11:56:30 +0000

    fix: remove variables

--- main.tf
+++ main.tf
@@ -158,9 +158,9 @@ module "webhook" {
   logging_kms_key_id                            = var.logging_kms_key_id
 
   # labels
-  enable_workflow_job_labels_check = var.runner_enable_workflow_job_labels_check
-  workflow_job_labels_check_all    = var.runner_enable_workflow_job_labels_check_all
-  runner_labels                    = local.runner_labels
+  # enable_workflow_job_labels_check = var.runner_enable_workflow_job_labels_check
+  # workflow_job_labels_check_all    = var.runner_enable_workflow_job_labels_check_all
+  # runner_labels                    = local.runner_labels
   role_path                        = var.role_path
   role_permissions_boundary        = var.role_permissions_boundary
   repository_white_list            = var.repository_white_list
